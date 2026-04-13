e9ade29548a9923ec1ea01020b97cb89334670a2
Pedro Jimenez <pjb1091@gmail.com>
2026-04-13 16:18:06 -0600

    [UPLOAD] Cambios al sistema

--- motor_firmas/urls.py
+++ motor_firmas/urls.py
@@ -21,24 +21,30 @@ urlpatterns = [
     path('portal/dashboard/', views.portal_dashboard, name='portal_dashboard'),
     path('portal/logout/', views.portal_logout, name='portal_logout'),
 
-    # RUTAS DE PLANTILLAS
+    # RUTAS DE PLANTILLAS (Cambiado a <str:> por los IDs de MongoDB)
     path('portal/plantillas/', views.portal_plantillas, name='portal_plantillas'),
-    path('portal/usar-plantilla/<int:plantilla_id>/', views.portal_usar_plantilla, name='portal_usar_plantilla'),
+    path('portal/usar-plantilla/<str:plantilla_id>/', views.portal_usar_plantilla, name='portal_usar_plantilla'),
 
     # === RUTAS DE PDFS LIBRES (DRAG & DROP) ===
     path('portal/mis-pdfs/', views.portal_pdfs_usuario, name='portal_pdfs_usuario'),
     path('portal/subir-pdf/', views.portal_subir_pdf, name='portal_subir_pdf'),
     path('api/subir-pdf-usuario/', views.subir_pdf_usuario, name='subir_pdf_usuario'),
-    path('api/eliminar-pdf-usuario/<int:pdf_id>/', views.eliminar_pdf_usuario, name='eliminar_pdf_usuario'),
-    path('portal/configurar-pdf/<int:pdf_id>/', views.portal_configurar_pdf, name='portal_configurar_pdf'),
+
+    # FIX: Cambiado de <int:pdf_id> a <str:pdf_id>
+    path('api/eliminar-pdf-usuario/<str:pdf_id>/', views.eliminar_pdf_usuario, name='eliminar_pdf_usuario'),
+    path('portal/configurar-pdf/<str:pdf_id>/', views.portal_configurar_pdf, name='portal_configurar_pdf'),
+
     path('api/iniciar-firma-libre/', views.iniciar_firma_libre, name='iniciar_firma_libre'),
 
     # RUTAS DEL PORTAL (ADMINISTRADORES)
     path('admin-portal/', views.admin_login, name='admin_login'),
     path('admin-portal/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('admin-portal/crear-plantilla/', views.admin_crear_plantilla, name='admin_crear_plantilla'),
-    path('admin-portal/editar-plantilla/<int:plantilla_id>/', views.admin_editar_plantilla,
+
+    # FIX: Cambiado de <int:plantilla_id> a <str:plantilla_id>
+    path('admin-portal/editar-plantilla/<str:plantilla_id>/', views.admin_editar_plantilla,
          name='admin_editar_plantilla'),
+
     path('admin-portal/logout/', views.admin_logout, name='admin_logout'),
     path('api/admin-action/<str:accion>/', views.admin_api, name='admin_api'),
 ]
