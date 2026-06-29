d39712d7c4075a726c4e2320ed4ea4bd2aa480ca
Pedro Jimenez B <pjimenezb@raloy.com.mx>
2026-06-29 15:01:55 -0600

    [UPLOAD] nuevos cambios a monitor de firmas

--- motor_firmas/migrations/0016_carpetadominio_branding.py
+++ motor_firmas/migrations/0016_carpetadominio_branding.py
@@ -10,24 +10,29 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='carpetadominio',
-            name='brand_color',
-            field=models.CharField(default='#162839', max_length=7),
-        ),
-        migrations.AddField(
-            model_name='carpetadominio',
-            name='logo_url',
-            field=models.CharField(blank=True, max_length=500, null=True),
-        ),
-        migrations.AddField(
-            model_name='carpetadominio',
-            name='logo_path',
-            field=models.CharField(blank=True, max_length=500, null=True),
-        ),
-        migrations.AddField(
-            model_name='carpetadominio',
-            name='updated_at',
-            field=models.DateTimeField(auto_now=True),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[],
+            state_operations=[
+                migrations.AddField(
+                    model_name='carpetadominio',
+                    name='brand_color',
+                    field=models.CharField(default='#162839', max_length=7),
+                ),
+                migrations.AddField(
+                    model_name='carpetadominio',
+                    name='logo_url',
+                    field=models.CharField(blank=True, max_length=500, null=True),
+                ),
+                migrations.AddField(
+                    model_name='carpetadominio',
+                    name='logo_path',
+                    field=models.CharField(blank=True, max_length=500, null=True),
+                ),
+                migrations.AddField(
+                    model_name='carpetadominio',
+                    name='updated_at',
+                    field=models.DateTimeField(auto_now=True),
+                ),
+            ],
         ),
     ]
